c602ffaddc0262f8dfc82ee71971bfa339398219
v5416080 <vaclav.kure@saint-gobain.com>
2021-10-04 16:12:07 +0200

    Převod na teplotu C

--- Projekty/HelloWord/HelloWord/Program.cs
+++ Projekty/HelloWord/HelloWord/Program.cs
@@ -4,35 +4,44 @@ namespace HelloWord
 {
     class Program
     {
-        const int c_a = 155;
-        const int c_b = 255;
+
         static void Main(string[] args)
         {
 
-            //Console.WriteLine("Hello World! - Ahoj, tady Václav.");
-            //AddAndPrint(5, 10000);
-            //Pozdrav("Karle");
-            Console.WriteLine("Vložte jméno:");
-            string input = Console.ReadLine();
-            if(input==null || input.Length < 1)
-            {
-                Console.WriteLine($"Není koho pozdravit");
-                return;
-            }
-            
-            if (input.ToLower() == "bob" || input.ToLower() == "alice")
-            {
-                Console.WriteLine("Vítej zpět!");
-                return;
-            }
-            Console.WriteLine($"Vítej {input}.");
+            int a = 23;
+            double d = 444.5698;
 
+            //Console.WriteLine($"a = {a}, (int)d = {(int)d}");
+            //Console.WriteLine($"a = {a}, Convert.ToInt32(d) = {Convert.ToInt32(d)}");
+            //Convert.ToInt32(2);
 
+            PrevodNaCelsius();
 
-           
-            
         }
 
+        private static void PrevodNaCelsius()
+        {
+            Console.WriteLine($"Vložte hodnotu ve °F:");
+            double farn = 0;
+            bool dotazovatSe = true;
+            while (dotazovatSe)
+            {
+                string vstup = Console.ReadLine();
+                
+                if(!double.TryParse(vstup, out farn))
+                {
+                    Console.WriteLine($"Hodnota {vstup} nelze převést. Zkuste znova:");
+                    continue;
+                }
+                dotazovatSe = false;
+            }
+
+            Console.WriteLine($"Výsledek: {farn} °F = {(farn-32d)/1.8d} °C");
+
+
+        }
+
+   
 
 
 
