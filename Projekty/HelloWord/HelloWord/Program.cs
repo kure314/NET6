9ea8316c2ba8f437566839e7c39148cd78e22914
v5416080 <vaclav.kure@saint-gobain.com>
2021-10-04 12:02:07 +0200

    AddAndPrint

--- Projekty/HelloWord/HelloWord/Program.cs
+++ Projekty/HelloWord/HelloWord/Program.cs
@@ -6,7 +6,13 @@ namespace HelloWord
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World! - Ahoj, tady Václav.");
+            //Console.WriteLine("Hello World! - Ahoj, tady Václav.");
+            AddAndPrint(5, 10000);
+        }
+        static int AddAndPrint(int a , int b)
+        {
+            Console.WriteLine(a + " + " + b + " = " + (a + b));
+            return a + b;
         }
     }
 }
