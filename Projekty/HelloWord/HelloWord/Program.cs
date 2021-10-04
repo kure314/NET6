54f102baca55663995cc491be1e12bc20b20a3d3
v5416080 <vaclav.kure@saint-gobain.com>
2021-10-04 15:25:09 +0200

    ReadLine

--- Projekty/HelloWord/HelloWord/Program.cs
+++ Projekty/HelloWord/HelloWord/Program.cs
@@ -8,40 +8,27 @@ namespace HelloWord
         const int c_b = 255;
         static void Main(string[] args)
         {
-        
+
             //Console.WriteLine("Hello World! - Ahoj, tady Václav.");
             //AddAndPrint(5, 10000);
             //Pozdrav("Karle");
-            int a = 20;
-            int b = 30;
-
-            int pom = a;
-            a = b;
-            b = pom;
-            decimal k = 5;
-            var kkde = 5M;
-
-            //Console.WriteLine($"a = {a}, b = {b}");
-            int vetsi = Vetsi(1, 2);
-            Console.WriteLine($"Větší číslo = {vetsi}.");
-        }
-
-
-        static int Vetsi(int a, int b)
-        {
-            if (a == c_a || a == c_b)
+            Console.WriteLine("Vložte jméno:");
+            string input = Console.ReadLine();
+            if(input==null || input.Length < 1)
             {
-                if (a > b) return b;
-                else return a;
+                Console.WriteLine($"Není koho pozdravit");
+                return;
             }
-            if (a > b)
-            {
-                return a;
-            }
-            return b;
-            
+            Console.WriteLine($"Zdravím  {input}.");
+
+
+
+           
             
         }
+
+
+
 
 
     }
