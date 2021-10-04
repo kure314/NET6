22e44b4915975bf3827ecd09bb6cdf8e8605ebd2
v5416080 <vaclav.kure@saint-gobain.com>
2021-10-04 14:42:02 +0200

    porovnání

--- Projekty/HelloWord/HelloWord/Program.cs
+++ Projekty/HelloWord/HelloWord/Program.cs
@@ -19,18 +19,28 @@ namespace HelloWord
             var kkde = 5M;
 
             //Console.WriteLine($"a = {a}, b = {b}");
-            Vetsi(1, 2);
+            int vetsi = Vetsi(1, 2);
+            Console.WriteLine($"Větší číslo = {vetsi}.");
         }
 
-        static void Vetsi(int a, int b)
+
+        static int Vetsi(int a, int b)
         {
             Console.WriteLine($"Přijato a = {a}, b = {b}");
             if (a > b)
-                Console.WriteLine($"Větší číslo je a. a = {a}.");
+            {
+                //Console.WriteLine($"Větší číslo je a. a = {a}.");
+                return a;
+            }
             if (a < b)
-                Console.WriteLine($"Větší číslo je b. b = {b}.");
-            if(a==b)
-                Console.WriteLine($"Obě čísla mají stejnou velikost. a = b = {a}.");
+            {
+                //Console.WriteLine($"Větší číslo je b. b = {b}.");
+                return b;
+            }
+
+            //Console.WriteLine($"Obě čísla mají stejnou velikost. a = b = {a}.");
+            return a;
+            
         }
 
 
